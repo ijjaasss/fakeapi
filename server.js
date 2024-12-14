04a9ccf5359bf79e6a1145bcd1e3cbf9593d61c1
ijjaasss <ijasmuthu04@gmail.com>
2024-12-14 17:23:34 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve the db.json file
+app.get('/users', (req, res) => {
+    const dbPath = path.join(__dirname, 'db.json');
+    fs.readFile(dbPath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading the database file');
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
