cbe4ae1258f053e19e7a40439f0745afb1fbaff3
ijjaasss <ijasmuthu04@gmail.com>
2024-12-24 17:41:42 +0530

    login

--- server.js
+++ server.js
@@ -34,6 +34,16 @@ app.get('/deliveryboy', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+app.get('/login', (req, res) => {
+    const dbPath = path.join(__dirname, 'login.json');
+    fs.readFile(dbPath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading the database file');
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
